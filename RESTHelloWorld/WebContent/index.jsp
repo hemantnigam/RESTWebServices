fb37ccb3037b59e261c10b1c022af6a1569a831b
hemantnigam <h.nigam654@gmail.com>
2017-08-14 07:38:23 -0400

    RESTWebService Basic Example for different parameter attributes

---
+++ RESTHelloWorld/WebContent/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Employee Form</title>
+</head>
+<body>
+<form action="http://localhost:8080/RESTHelloWorld/rest/customers/addCustomer/" method="post">
+Name:<input type="text" name="name"><br>
+Country:<input type="text" name="country"><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ RESTHelloWorld/src/com/api/RESTHelloWorld.java
@@ -0,0 +1,54 @@
+package com.api;
+
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/customers/")
+public class RESTHelloWorld {
+
+	@GET
+	@Produces("text/html")
+	public Response getLocalCust() {
+		String output="Local Constructor";
+		return Response.status(200).entity(output).build();
+	}
+	@GET
+	@Produces("text/html")
+	@Path("/nri")
+	public Response getNriCust() {
+		return Response.status(200).entity("Local Method").build();
+	}
+	@GET
+	@Produces("text/html")
+	@Path("/{name}/{country}")
+	public Response getURLParameterValue(@PathParam("name") String name,@PathParam("country") String country) {
+	return Response.status(200).entity("Name ="+name+"Country"+country).build(); 
+	}
+	@GET
+	@Produces("text/html")
+	@Path("/details/")
+	public Response getURLQueryValue(@QueryParam("name") String name,@QueryParam("country") String country) {
+		return Response.status(200).entity("Name ="+name+"\tCountry="+country).build(); 
+	}
+	@GET
+	@Produces("text/html")
+	@Path("/values/")
+	public Response getURLmatrixValue(@MatrixParam("name") String name,@MatrixParam("country") String country) {
+		return Response.status(200).entity("Name ="+name+"\tCountry="+country).build(); 
+	}
+	@POST
+	@Produces("text/html")
+	@Path("/addCustomer")
+	public Response getFormValue(@FormParam("name") String name,@FormParam("country") String country) {
+		return Response.status(200).entity("Name ="+name+"\tCountry="+country).build(); 
+	}
+}
